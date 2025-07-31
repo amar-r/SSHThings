52de6ea140adadf3a6f2b0eedb366e81744d026c
amar-r <amar-r@users.noreply.github.com>
2025-07-30 21:55:54 -0400

    Simplify RaspberryPI-Gration post to only show loading bar

--- posts/raspberrypi-gration.md
+++ posts/raspberrypi-gration.md
@@ -8,16 +8,4 @@ readTime: 5
 
 # The Great RaspberryPI-Gration
 
-## Introduction
-
-This is a placeholder for the upcoming blog post about Raspberry Pi migration adventures.
-
-## Coming Soon
-
-Loading migration content...
-
-More content will be added here soon...
-
----
-
-*Placeholder for future content about Raspberry Pi migration experiences.* 
+Loading migration content... 
--- src/utils/posts.js
+++ src/utils/posts.js
@@ -7,19 +7,7 @@ export const posts = [
     excerpt: 'Why my Raspberry PIs are all stashed in my \'I\'ll use those wires one day\' box',
     content: `# The Great RaspberryPI-Gration
 
-## Introduction
-
-This is a placeholder for the upcoming blog post about Raspberry Pi migration adventures.
-
-## Coming Soon
-
-Loading migration content...
-
-More content will be added here soon...
-
----
-
-*Placeholder for future content about Raspberry Pi migration experiences.*`,
+Loading migration content...`,
     tags: ['raspberry-pi', 'migration', 'homelab', 'self-hosting'],
     readTime: 5,
     image: null
