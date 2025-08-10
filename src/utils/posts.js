bf725d5be9208335a0a208653e77afaf1bc25565
amar-r <amar-r@users.noreply.github.com>
2025-08-09 22:58:14 -0400

    Update Cloudflare blog post with improved content and structure
    - Add TL;DR section for quick summary
    - Improve Netlify section with private repo mention
    - Add GitHub Pages private repo limitation note
    - Add Cloudflare Pages private repo capability
    - Fix markdown link formatting throughout
    - Streamline conclusion and improve readability

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -12,9 +12,10 @@ export const posts = [
     excerpt: 'Switching to Cloudflare was one of the best things I did for my portfolio site.',
     content: `> I initially titled this post "Why you should use Cloudflare pages" but then at the end realized it's more "Why you should use Cloudflare". I'm 100% positive I'm not even going to be scratching the surface with what Cloudflare can provide. But this is what I've learned over a short period of time. I hope to learn more and implement more.
 
-## Synopsis
+## TL;DR
 
-I wanted to start hosting a portfolio page to provide a little more infomation about myself that LinkedIn couldn't quite fulfill. I wanted something that was essentially free, exluding the domain purchase of course. Below I talk about how I found the best solution for me. Free, secure, and privacy focused.
+* Needed a free, privacy-friendly way to host a portfolio.
+* Chose Cloudflare Pages—simple Git deploys, SSL by default, and zero cost for my needs.
 
 ## The Journey
 
@@ -24,11 +25,11 @@ Circa 2018, I had a technical blog around SCCM/MECM. I hosted it on AWS under th
 
 ### Netlify
 
-I actually had never heard of Netlify. My consultant (AI) told me about it and I just went with it. I was excited to have something back up and running for the public to see. Netlify was cool, it allowed me to use a custom domain (Amar-r.com) and GitOps for free. Which means, anytime I posted a new commit to my main branch a new deployment was kicked off in Netlify and updated my page. It handled SSL and a few other security functionalities out of the box with minimal clicks. It was nice to use. I'm sure I only touched a small part of what I could do with it. Mabye one day I'll go back and investigate further. Feel free to check it out at [Netlify.com](https://www.netlify.com/).
+I actually had never heard of Netlify. My consultant (AI) told me about it and I just went with it. I was excited to have something back up and running for the public to see. Netlify was cool, it allowed me to use a custom domain (Amar-r.com) and GitOps for free. Which means, anytime I posted a new commit to my main branch a new deployment was kicked off in Netlify and updated my page. It handled SSL and a few other security functionalities out of the box with minimal clicks. It was nice to use. I could use a private repo, which was a nice feature. Mabye one day I'll go back and investigate further. Feel free to check it out at [Netlify.com](https://www.netlify.com/).
 
 ### GitHub Pages
 
-I actually have this blog posted on [GitHub Pages](https://docs.github.com/en/pages/getting-started-with-github-pages/what-is-github-pages), because variety is good. When I thought about it, I was like why not use GitHub Pages and more importantly [GitHub Actions](https://github.com/features/actions). Once you get the hang of it, it's pretty cool. All your source code is in one place so why not have the site hosted in the same spot. Creating the deploy.yml file was pretty straightforward and kinda a breath of fresh air after using Jenkins and groovy since ~2020 🤮.
+I actually have this blog posted on [GitHub Pages](https://docs.github.com/en/pages/getting-started-with-github-pages/what-is-github-pages), because variety is good. When I thought about it, I was like why not use GitHub Pages and more importantly [GitHub Actions](https://github.com/features/actions). Once you get the hang of it, it's pretty cool. All your source code is in one place so why not have the site hosted in the same spot. Creating the deploy.yml file was pretty straightforward and kinda a breath of fresh air after using Jenkins and groovy since ~2020 🤮. GitHub Pages does NOT allow you to have private repos unless you want to pay for it.
 
 ### Cloudflare Pages
 
@@ -36,9 +37,9 @@ I migrated my domain from Namecheap over to Cloudflare for...reasons. I like the
 
 I've always been a big propoent of security and privacy. I don't like my data or information to be out there for people to find. When I created my portfoio page, I decided to provide more information than I typically would. I had my entire resume posted on my site, which included the city and state of my current residence, my email, and my phone number. Yes, you read that correctly. I put my resume with my personal phone number on a publically facing website.
 
-This is when I started to really like [Cloudflare Pages](https://pages.cloudflare.com/) and a feature within Cloudflare itself, called [Cloudflare Zero Trust](https://developers.cloudflare.com/cloudflare-one/) one-time PIN (OTP). Before you say anything, you don't have to use Cloudflare Pages to utlilize the Cloudflare Zero Trust features, you only need your domain to be managed by Cloudflare.
+This is when I started to really like [Cloudflare Pages](https://pages.cloudflare.com/) and a feature within Cloudflare itself, called [Cloudflare Zero Trust](https://developers.cloudflare.com/cloudflare-one/) one-time PIN (OTP). Before you say anything, you don't have to use Cloudflare Pages to utlilize the Cloudflare Zero Trust features, you only need your domain to be managed by Cloudflare. Btw, you can use private repos with Cloudflare Pages.
 
-The OTP feature really came in handy for me because I was able to have my resume posted on my site but with a little extra security. In order for someone to view my resume, they have to enter their email and then enter a PIN which was sent to them via Cloudflare. Much better solution than just having it readily available. Yes, I know it's still easy for someone to view my entire resume. I'm most likely going to setup Cloudflare's feature where I get an email to approve requests people put in to gain access, which is timelocked btw to a specific time.
+The OTP feature really came in handy for me because I was able to have my resume posted on my site but with a little extra security. In order for someone to view my resume, they have to enter their email and then enter a PIN which was sent to them via Cloudflare. Much better solution than just having it readily available. Yes, I know it's still easy for someone to view my entire resume. I'm most likely going to setup Cloudflare's feature where I get an email to approve requests people put in to gain access, which is timelocked to a specific time.
 
 Please go checkout what they have to offer from a securitiy standpoint. There are other features I implemented like HSTS to force browsers to communicate with my site strictly over HTTPS.`,
     tags: ['security', 'privacy', 'AI', 'self-promotion'],
